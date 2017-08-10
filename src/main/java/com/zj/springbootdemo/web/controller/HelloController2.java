cdc2b4e552a2927e28ec7cd72575ccea0b02894b
Zhang Jin <jin.zhang@fuwo.com>
2017-08-10 11:22:35 +0800

    commit

---
+++ src/main/java/com/zj/springbootdemo/web/controller/HelloController2.java
@@ -0,0 +1,15 @@
+package com.zj.springbootdemo.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloController2 {
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public String index() {
+        return "index";
+    }
+}
+
